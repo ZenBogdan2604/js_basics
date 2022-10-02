f05314b0006ccaa2bb5d1167b7f9ac01ded649f1
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-10-02 11:49:05 +0300

    add basic operations

--- index.js
+++ index.js
@@ -20,11 +20,32 @@ console.log(message)*/
     Undefind
 */
 
-var number = 42
+/*var number = 42
 var string = 'Message'
 var isTrue = true // false
 var obj = {a: 1}
 var nothing = null
 var undef = undefined
 
-console.log(typeof number)
+console.log(typeof number)*/
+
+var num1 = 12
+var num2 = 8
+
+console.log('+:', num1 + num2)
+console.log('-:', num1 - num2)
+console.log('*:', num1 * num2)
+console.log('/:', num1 / num2)
+
+var str1 = 'ZA'
+var str2 = 'WARUDO!'
+
+console.log('+:', str1 + ' ' + str2)
+
+console.log('1 + 2:', 1 + '2')
+console.log('str1 + num1:', str1 + num1)
+
+console.log('Bollean + string:', true + str2)
+
+console.log('True + 1:', true + 1)
+console.log('False + 1:', false + 1)
