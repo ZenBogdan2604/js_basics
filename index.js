878d7708dc3fd6954a7edfd3ed4591deef4f1ae7
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-10-23 12:52:44 +0300

    add function

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ if (Nan) {
     console.log('Значение false')
 }*/
 
-var carColor = 'green'
+/*var carColor = 'green'
 
 switch (carColor) {
     case 'green':
@@ -141,3 +141,25 @@ switch (carColor) {
     default:
         console.log('Цвет неопределён')
     }
+*/
+
+var carName = 'Ford'
+var carYear = 2010
+var personYear = 1990
+
+function calculateAge(year) {
+    var currentYear = 2018
+    var result = currentYear - year
+    return result
+}
+
+function checkAngLogAge(year) {
+    if(calculateAge(carYear) < 10) {
+        console.log('Возраст меньше 10 лет')
+    } else {
+        console.log('Возраст больше 10 лет')
+    }
+}
+
+checkAngLogAge(carYear)
+checkAngLogAge(personYear)
