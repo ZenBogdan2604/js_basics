e6058fba5755e397d4d3a3c34e3c476a2ff59e90
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-11-02 11:53:37 +0300

    input

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ console.log(h1)*/
 var div = document.querySelector('#playground')
 var p = document.querySelectorAll('.text')
 var h1 = document.querySelector
-var il = document.querySelector('div#playground ul')
+var input = document.querySelector('input')
+div.innerHTML = '<h2 style="color: red;">From javascript</h2>'
+h1.textContent = 'Changed from JS'
+//var il = document.querySelector('div#playground ul')
+console.log(input.value)
 console.log(div)
 console.log(p)
-console.log(h1)
-console.log(ul)
+console.log(h1.textContent)
+//console.log(ul)
