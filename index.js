20b3d5483fcc0e2cfbdb14699ebe3ea43961c561
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-11-02 12:26:52 +0300

    attributes

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-/*var div = document.getElementById('playground')
-var p = document.getElementsByClassName('')
-var h1 = document.getElementsByTagName
-console.log(div)
-console.log(p)
-console.log(h1)*/
+var a = document.querySelector('a')
+var oldHref = a.getAttribute('href')
 
-var div = document.querySelector('#playground')
-var p = document.querySelectorAll('.text')
-var h1 = document.querySelector
-var input = document.querySelector('input')
-div.innerHTML = '<h2 style="color: red;">From javascript</h2>'
-h1.textContent = 'Changed from JS'
-//var il = document.querySelector('div#playground ul')
-console.log(input.value)
-console.log(div)
-console.log(p)
-console.log(h1.textContent)
-//console.log(ul)
+a.setAttribute('href', 'https://ya.ru')
+a.setAttribute('title', 'Go to yandex')
+a.textContent = 'Yandex'
+
+console.log(a.attributes)
