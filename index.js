b52b363f12af1871d69cfff51829d8448b22f1fc
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-11-03 15:35:15 +0300

    classes

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-/*var a = document.querySelector('a')
-var oldHref = a.getAttribute('href')
 
-a.setAttribute('href', 'https://ya.ru')
-a.setAttribute('title', 'Go to yandex')
-a.textContent = 'Yandex'
-
-console.log(a.attributes)*/
-
-/*var students = {
-    name: '',
-    age: 0,
-    phoneNumber: ''
-    
-//}
 
 
 
@@ -57,7 +43,7 @@ for (var step = 1;step <= 3; step++){
         phones.storage = 256
     }
     console.log(phones)
-}*/
+}
 
 var cars = {
     id: '',
@@ -84,5 +70,26 @@ for (var step = 1;step <= 4; step++){
         cars.model = 'c43'
     }
     console.log(cars)
-}
+}*/
 
+var a = document.querySelector('a')
+var oldHref = a.getAttribute('href')
+
+a.setAttribute('href', 'https://ya.ru')
+a.setAttribute('title', 'Go to yandex')
+a.textContent = 'Yandex'
+
+
+var box1 = document.querySelector('#box1')
+var box2 = document.querySelector('#box2')
+
+box1.classList.add('red')
+
+var hasClass = box2.classList.contains('blue')
+console.log(hasClass)
+
+if (hasClass) {
+  box2.classList.remove('blue')
+} else {
+  box2.classList.add('blue')
+}
