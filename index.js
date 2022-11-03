59e048a9c2e4d8197230b4347651edef11046a74
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-11-03 15:07:14 +0300

    objects and cycle

--- index.js
+++ index.js
@@ -1,8 +1,88 @@
-var a = document.querySelector('a')
+/*var a = document.querySelector('a')
 var oldHref = a.getAttribute('href')
 
 a.setAttribute('href', 'https://ya.ru')
 a.setAttribute('title', 'Go to yandex')
 a.textContent = 'Yandex'
 
-console.log(a.attributes)
+console.log(a.attributes)*/
+
+/*var students = {
+    name: '',
+    age: 0,
+    phoneNumber: ''
+    
+//}
+
+
+
+
+/*for (var step = 1;step <= 3; step++){
+    if (step == 1){
+        students.name = 'Вася',
+        students.age = 14,
+        students.phoneNumber = '+79647386714'
+        for (idphone = 1)
+        
+    } else if (step == 2){
+        students.name = 'Олег',
+        students.age = 13,
+        students.phoneNumber = '+79854601372'
+    } else if(step == 3){
+        students.name = 'Саня',
+        students.age = 15,
+        students.phoneNumber = '+79735942949'
+    }
+    console.log(students)
+}
+
+var phones = {
+    name: '',
+    model: '',
+    storage: 0    
+}
+
+for (var step = 1;step <= 3; step++){
+    if (step === 1){
+        phones.name = 'IPhone',
+        phones.model = '9',
+        phones.storage = 128
+    } else if (step === 2){
+        phones.name = 'Realme',
+        phones.model = 'C21',
+        phones.storage = 64
+    }else if (step === 3){
+        phones.name  = 'Samsung',
+        phones.model = 'J20',
+        phones.storage = 256
+    }
+    console.log(phones)
+}*/
+
+var cars = {
+    id: '',
+    name: '',
+    model: ''
+}
+
+for (var step = 1;step <= 4; step++){
+    if (step === 1){
+        cars.id = step,
+        cars.name = 'Lada',
+        cars.model = '4'
+    }else if (step === 2){
+        cars.id = step,
+        cars.name = 'Audi',
+        cars.model = 's4'
+    }else if (step === 3){
+        cars.id = step,
+        cars.name = 'BMW',
+        cars.model = 'x7'
+    }else if (step === 4){
+        cars.id = step,
+        cars.name = 'Mercedes',
+        cars.model = 'c43'
+    }
+    console.log(cars)
+}
+
