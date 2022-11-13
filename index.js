6cace303580bcaa8268e1dab7dd07e48b9947ba9
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2022-11-13 07:51:41 +0300

    add event

--- index.js
+++ index.js
@@ -144,10 +144,31 @@ h1.addEventListener('mouseleave', function() {
 button.addEventListener('click', buttonHandler)*/
 
 var divs = document.querySelectorAll('div')
-
+var link = document.querySelector('a')
+ 
 for (var i = 0; 1 < divs.length; i++) {
-    divs[i].addEventListener('click', function() {
+    divs[i].addEventListener('click', function(event) {
+        event.stopPropagation()
         console.log(this.getAttribute('id'))
     })
 }
 
+link.addEventListener('click', handleLinkClick)
+
+function handleLinkClick(event) {
+    event.preventDefault()
+
+    var div = divs[0]
+
+    if (div.style.display === 'none') {
+        div.style.display = 'flex'
+    } else {
+        div.style.display = 'none'
+    }
+
+    div.style.display = div.style.display === 'none'
+    ? 'flex'
+    : 'none'
+
+    console.log(div.style.display)
+}
